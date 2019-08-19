82415c112043f3114e0dfd7132c6013788809e40
James Volmert <james.volmert@gmail.com>
2019-08-19 17:28:06 -0600

    added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <div class="nav-wrapper purple darken-1">
+            <router-link to="">
+                <span class="brand-logo">
+                    Ninja Smoothies
+                </span>
+            </router-link>
+            <a href="" class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+                <router-link to="">
+                    <i class="material-icons">
+                        add
+                    </i>
+                </router-link>
+            </a>
+        </div>
+    </nav>
+    
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
